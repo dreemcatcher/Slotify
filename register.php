41f7f55bc28b160ba023e7446a7e8110df865c62
dreemcatcher <etheralgreen@gmail.com>
2019-10-04 03:20:08 +1100

    Register data updated

--- register.php
+++ register.php
@@ -12,13 +12,49 @@
       <h2>Login to your account</h2>
       <p>
         <label for="Username">Username</label>
-      <input id="loginUsername" name="loginUsername" type="text" placeholder="username" required>
+        <input id="loginUsername" name="loginUsername" type="text" placeholder="username" required>
       </p>
       <p>
-         <label for="Password">Password</label>
-      <input id="loginPassword" name="loginPassword" type="Password" required>
+        <label for="Password">Password</label>
+        <input id="loginPassword" name="loginPassword" type="Password" required>
       </p>
+      <button type="submit" name="loginButton">LOG IN</button>
     </form>
+
+
+    <form id="registerForm" action="register.php" method="POST">
+      <h2>Create your free account</h2>
+      <p>
+        <label for="username">Username</label>
+        <input id="username" name="username" type="text" placeholder="username" required>
+      </p>
+      <p>
+        <label for="firstName">First name</label>
+        <input id="firstName" name="firstName" type="text" placeholder="First name" required>
+      </p>
+      <p>
+        <label for="lastName">Last name</label>
+        <input id="lastName" name="lastName" type="text" placeholder="Last name" required>
+      </p>
+      <p>
+        <label for="email">Email</label>
+        <input id="email" name="email" type="email" placeholder="email@mail.ru" required>
+      </p>
+      <p>
+        <label for="email2">Confirm email</label>
+        <input id="email2" name="email2" type="text" placeholder="username" required>
+      </p>
+      <p>
+        <label for="password">Password</label>
+        <input id="password" name="password" type="Password" required>
+      </p>
+      <p>
+        <label for="password2">Password</label>
+        <input id="password2" name="password2" type="Password" required>
+      </p>
+      <button type="submit" name="loginButton">LOG IN</button>
+    </form>
+
   </div>
 </body>
 </html>
