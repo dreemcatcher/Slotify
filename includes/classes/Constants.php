14949de1e4e952eaf21bcee02f5ae1263f51024c
dreemcatcher <etheralgreen@gmail.com>
2019-10-04 05:24:49 +1100

    Add valudation class and others

---
+++ includes/classes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+class Constants{
+	public static $passwordsDoNotMatch = "Your passwords dont match";
+	public static $passwordNotAlphanumeric = "Your password can only contain numbers and symbols";
+	public static $passwordCharacters = "Your password must be between 5 and 30 characters";
+	public static $emailInvalid = "Your emails is invalid";
+	public static $emailDoNotMatch = "Your emails dont match";
+	public static $lastNameCharacters = "Your Last Name must be between 25 and 1 characters long";
+	public static $firstNameCharacters = "Your First Name must be between 25 and 2 characters long";
+	public static $usernameCharacters =  "Your username must be between 25 and 4 characters long";
+}
+?>
--- register.php
+++ register.php
@@ -1,46 +1,11 @@
 <?php
-function sanitizeFormPassword($inputText){
-  $inputText = strip_tags($inputText);
-  return $inputText;
-}
+  include("includes/classes/Account.php");
+  include("includes/classes/Constants.php");
+  $account = new Account();
 
-function sanitizeFormUsername($inputText){
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  return $inputText;
-}
-
-function sanitizeFormString($inputText){
-  $inputText = strip_tags($inputText);
-  $inputText = str_replace(" ", "", $inputText);
-  $inputText = ucfirst(strtolower($inputText));
-  return $inputText;
-}
-
-if(isset($_POST['loginButton'])){
-  //echo "loginbutton was pressed";
-}
-
-if(isset($_POST['registerButton'])){
-  //echo "registerButton was pressed";
-  $username = sanitizeFormUsername($_POST['username']);
-
-  $firstName = sanitizeFormString($_POST['firstName']);
-
-  $lastName = sanitizeFormString($_POST['lastName']);
-
-  $email = sanitizeFormString($_POST['email']);
-  
-  $email2 = sanitizeFormString($_POST['email2']);
-
-  $password =sanitizeFormPassword($_POST['password']);
-
-  $password2=sanitizeFormPassword($_POST['password2']);
-
-  //echo "username";
-}
+  include("includes/handlers/register-handler.php");
+  include("includes/handlers/login-handler.php");
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -66,18 +31,23 @@ if(isset($_POST['registerButton'])){
     <form id="registerForm" action="register.php" method="POST">
       <h2>Create your free account</h2>
       <p>
+        <?php echo $account->getError(Constants::$usernameCharacters); ?>
         <label for="username">Username</label>
         <input id="username" name="username" type="text" placeholder="username" required>
       </p>
       <p>
+        <?php echo $account->getError(Constants::$firstNameCharacters); ?>
         <label for="firstName">First name</label>
         <input id="firstName" name="firstName" type="text" placeholder="First name" required>
       </p>
       <p>
+        <?php echo $account->getError(Constants::$lastNameCharacters); ?>
         <label for="lastName">Last name</label>
         <input id="lastName" name="lastName" type="text" placeholder="Last name" required>
       </p>
       <p>
+        <?php echo $account->getError(Constants::$emailDoNotMatch); ?>
+        <?php echo $account->getError(Constants::$emailInvalid); ?>
         <label for="email">Email</label>
         <input id="email" name="email" type="email" placeholder="email@mail.ru" required>
       </p>
@@ -86,11 +56,14 @@ if(isset($_POST['registerButton'])){
         <input id="email2" name="email2" type="text" placeholder="username" required>
       </p>
       <p>
+        <?php echo $account->getError(Constants::$passwordsDoNotMatch); ?>
+        <?php echo $account->getError(Constants::$passwordNotAlphanumeric); ?>
+        <?php echo $account->getError(Constants::$passwordCharacters); ?>
         <label for="password">Password</label>
         <input id="password" name="password" type="Password" placeholder="YUOR Password" required>
       </p>
       <p>
-        <label for="password2">Password</label>
+        <label for="password2">Confirm password</label>
         <input id="password2" name="password2" type="Password" placeholder="YUOR Password" required>
       </p>
       <button type="submit" name="registerButton">SIGN UP</button>
