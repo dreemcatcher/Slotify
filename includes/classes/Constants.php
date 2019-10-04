03d31d8aa37c757981321caed07f5fdb80464eed
dreemcatcher <etheralgreen@gmail.com>
2019-10-04 19:47:39 +1100

    Login function now available

--- includes/classes/Constants.php
+++ includes/classes/Constants.php
@@ -7,8 +7,10 @@ class Constants{
 	public static $emailDoNotMatch = "Your emails dont match";
 	public static $lastNameCharacters = "Your Last Name must be between 25 and 1 characters long";
 	public static $firstNameCharacters = "Your First Name must be between 25 and 2 characters long";
-	public static $usernameCharacters =  "Your username must be between 25 and 4 characters long";
-	public static $usernameTaken =  "This username already exist";
-	public static $emailTaken =  "This Email already in use";
+	public static $usernameCharacters = "Your username must be between 25 and 4 characters long";
+	public static $usernameTaken = "This username already exist";
+	public static $emailTaken = "This Email already in use";
+
+	public static $loginFailed = "Yuor username or password are incorrect";
 }
 ?>
--- includes/handlers/login-handler.php
+++ includes/handlers/login-handler.php
@@ -4,6 +4,10 @@ if(isset($_POST['loginButton'])){
 	$username = $_POST['loginUsername'];
 	$password = $_POST['loginPassword'];
 
-	//Login Function
+	$result = $account->login($username, $password);
+
+	if($result == true){
+		header("Location: index.php");
+	}
 }
 ?>
